d98b427f6a357584251c41de5e782a99f3d35b49
Lex Brouwers <lexbrouwers@coffeeit.nl>
2022-06-17 13:49:26 +0200

    Adjust config examples a little more

--- Sources/CITPincode/CITPincodeConfig.swift
+++ Sources/CITPincode/CITPincodeConfig.swift
@@ -61,7 +61,7 @@ extension CITPincodeConfig {
     public static var socialBlox = CITPincodeConfig(
         codeLength: 6,
         font: .system(size: 28, weight: .bold), // Font.custom(CITFont.Lato.bold.name, size: 28) & lineheight .size(24)
-        textColor: .white,
+        textColor: Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)),
         errorColor: Color(#colorLiteral(red: 0.968627451, green: 0.4117647059, blue: 0.2901960784, alpha: 1)),
         backgroundColor: Color(#colorLiteral(red: 0.2117647059, green: 0.2039215686, blue: 0.2392156863, alpha: 1)),
         selectedBackgroundColor: Color(#colorLiteral(red: 0.2117647059, green: 0.2039215686, blue: 0.2392156863, alpha: 1)),
@@ -76,7 +76,7 @@ extension CITPincodeConfig {
     public static var bagtag = CITPincodeConfig(
         codeLength: 6,
         font: .system(size: 16),
-        textColor: .white,
+        textColor: Color(#colorLiteral(red: 0.1215686275, green: 0.1960784314, blue: 0.3529411765, alpha: 1)),
         errorColor: Color(#colorLiteral(red: 0.937254902, green: 0.3137254902, blue: 0.3137254902, alpha: 1)),
         backgroundColor: Color(#colorLiteral(red: 0.9607843137, green: 0.9607843137, blue: 0.9607843137, alpha: 1)),
         selectedBackgroundColor: Color(#colorLiteral(red: 0.9607843137, green: 0.9607843137, blue: 0.9607843137, alpha: 1)),
