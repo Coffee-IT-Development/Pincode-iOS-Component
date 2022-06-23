a2ba8136080ed545dc5a71fc000b35f314ced505
Lex Brouwers <lexbrouwers@coffeeit.nl>
2022-06-23 14:42:09 +0200

    Expose resend button style

--- Sources/CITPincode/CITPincodeResendButtonStyle.swift
+++ Sources/CITPincode/CITPincodeResendButtonStyle.swift
@@ -9,14 +9,14 @@ import Foundation
 import SwiftUI
 
 public struct CITPincodeResendButtonStyle: Equatable {
-    var text: String = "Send code again"
-    var font: Font
-    var textColor: Color
-    var backgroundColor: Color
-    var contentInsets: EdgeInsets = CITPincodeResendButtonStyle.defaultContentInsets
-    var cornerRadius: CGFloat = .infinity
-    var cooldown: CITPincodeResendCodeCooldown = .none
-    var alignment: HorizontalAlignment = .leading
+    public var text: String = "Send code again"
+    public var font: Font
+    public var textColor: Color
+    public var backgroundColor: Color
+    public var contentInsets: EdgeInsets = CITPincodeResendButtonStyle.defaultContentInsets
+    public var cornerRadius: CGFloat = .infinity
+    public var cooldown: CITPincodeResendCodeCooldown = .none
+    public var alignment: HorizontalAlignment = .leading
     
     public static let defaultContentInsets: EdgeInsets = .init(top: 8, leading: 16, bottom: 8, trailing: 16)
     public static let none = CITPincodeResendButtonStyle(font: .system(size: 14), textColor: .clear, backgroundColor: .clear)
