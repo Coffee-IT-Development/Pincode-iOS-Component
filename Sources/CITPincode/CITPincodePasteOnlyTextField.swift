d9f945ced0f9bef51df81cfae17e8339e22fe0c3
Lex Brouwers <lexbrouwers@coffeeit.nl>
2022-07-01 14:21:19 +0200

    Try only allowing paste

--- Sources/CITPincode/CITPincodePasteOnlyTextField.swift
+++ Sources/CITPincode/CITPincodePasteOnlyTextField.swift
@@ -9,9 +9,6 @@ import UIKit
 
 public class CITPincodePasteOnlyTextField: UITextField {
     public override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
-//        if action == #selector(UIResponderStandardEditActions.paste(_:)) {
-//            return false
-//        }
-        return super.canPerformAction(action, withSender: sender)
+        action == #selector(UIResponderStandardEditActions.paste(_:))
     }
 }
