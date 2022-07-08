7871ac5f6735b6e5892f67bdf5791913b219b93a
Lex Brouwers <lexbrouwers@coffeeit.nl>
2022-07-08 12:03:09 +0200

    Expose some of the CITEditMenuHelper code

--- Sources/CITPincode/Internal/Sixteen/CITEditMenuHelper.swift
+++ Sources/CITPincode/Internal/Sixteen/CITEditMenuHelper.swift
@@ -10,22 +10,22 @@ import SwiftUI
 // TODO: Uncomment code below to support iOS 16 when it's released. Expected release 13-20 September (2022) as usual.
 //
 //@available(iOS 16.0, *)
-//class CITEditMenuHelper {
-//    static var shared = CITEditMenuHelper()
+class CITEditMenuHelper {
+    static var shared = CITEditMenuHelper()
 //    
 //    private let pasteActionMenu = PasteActionMenu()
 //    
-//    func setupPasteEditMenu(for inputField: UITextField) {
+    func setupPasteEditMenu(for inputField: UITextField) {
 //        inputField.addInteraction(pasteActionMenu.editMenuInteraction)
-//    }
+    }
 //    
-//    func showEditMenu(in frame: CGRect) {
+    func showEditMenu(in frame: CGRect) {
 ////        let configuration = UIEditMenuConfiguration(identifier: nil, sourcePoint: .init(x: frame.midX, y: 0))
 //        pasteActionMenu.editMenuInteraction.presentEditMenu(with: configuration)
-//    }
+    }
 //}
 //
 //@available(iOS 16.0, *)
 //class PasteActionMenu: NSObject, UIEditMenuInteractionDelegate {
 //    lazy var editMenuInteraction = UIEditMenuInteraction(delegate: self)
-//}
+}
