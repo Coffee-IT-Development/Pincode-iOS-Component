591a15177110f7a4fe63446f5d6c122e62eb68b6
Lex Brouwers <lexbrouwers@coffeeit.nl>
2022-07-08 11:48:22 +0200

    Fix issues for Xcode 13

--- Sources/CITPincode/CITPincodeResendCodeCooldown.swift
+++ Sources/CITPincode/CITPincodeResendCodeCooldown.swift
@@ -5,7 +5,7 @@
 //  Created by Lex Brouwers on 10/06/2022.
 //
 
-import Foundation
+import UIKit
 
 /// When the resendCode button is pressed, it is disabled for the given cooldown duration, can be none or any non-nil absolute value.
 public enum CITPincodeResendCodeCooldown: Equatable {
--- Sources/CITPincode/Internal/Sixteen/CITEditMenuHelper.swift
+++ Sources/CITPincode/Internal/Sixteen/CITEditMenuHelper.swift
@@ -7,23 +7,25 @@
 
 import SwiftUI
 
-@available(iOS 16.0, *)
-class CITEditMenuHelper {
-    static var shared = CITEditMenuHelper()
-    
-    private let pasteActionMenu = PasteActionMenu()
-    
-    func setupPasteEditMenu(for inputField: UITextField) {
-        inputField.addInteraction(pasteActionMenu.editMenuInteraction)
-    }
-    
-    func showEditMenu(in frame: CGRect) {
-        let configuration = UIEditMenuConfiguration(identifier: nil, sourcePoint: .init(x: frame.midX, y: 0))
-        pasteActionMenu.editMenuInteraction.presentEditMenu(with: configuration)
-    }
-}
-
-@available(iOS 16.0, *)
-class PasteActionMenu: NSObject, UIEditMenuInteractionDelegate {
-    lazy var editMenuInteraction = UIEditMenuInteraction(delegate: self)
-}
+// TODO: Uncomment code below to support iOS 16 when it's released. Expected release 13-20 September (2022) as usual.
+//
+//@available(iOS 16.0, *)
+//class CITEditMenuHelper {
+//    static var shared = CITEditMenuHelper()
+//    
+//    private let pasteActionMenu = PasteActionMenu()
+//    
+//    func setupPasteEditMenu(for inputField: UITextField) {
+//        inputField.addInteraction(pasteActionMenu.editMenuInteraction)
+//    }
+//    
+//    func showEditMenu(in frame: CGRect) {
+////        let configuration = UIEditMenuConfiguration(identifier: nil, sourcePoint: .init(x: frame.midX, y: 0))
+//        pasteActionMenu.editMenuInteraction.presentEditMenu(with: configuration)
+//    }
+//}
+//
+//@available(iOS 16.0, *)
+//class PasteActionMenu: NSObject, UIEditMenuInteractionDelegate {
+//    lazy var editMenuInteraction = UIEditMenuInteraction(delegate: self)
+//}
