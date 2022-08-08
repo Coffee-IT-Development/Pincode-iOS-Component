305aa1ee6ad2091a851152d4a056c7dfe271fbab
Lex Brouwers <lexbrouwers@coffeeit.nl>
2022-08-08 20:36:45 +0200

    Remove commented out init

--- Sources/CITPincode/CITPincodeConfig.swift
+++ Sources/CITPincode/CITPincodeConfig.swift
@@ -71,49 +71,6 @@ public struct CITPincodeConfig: Equatable {
     
     /// Optional config used to show a resendButton, meant to resend an One Time Passcode on press and is automatically disabled for a given cooldown duration to limit usage.
     public var resendButton: CITPincodeResendButtonConfig = .none
-    
-//    public init(
-//        codeLength: Int = 6,
-//        font: Font = .system(size: 16),
-//        errorFont: Font? = nil,
-//        textColor: Color = .black,
-//        errorColor: Color = .red,
-//        placeholder: String = "",
-//        placeholderColor: Color? = nil,
-//        backgroundColor: Color = .init(white: 0.2),
-//        selectedBackgroundColor: Color? = nil,
-//        selectedBorderColor: Color? = nil,
-//        selectedBorderWidth: CGFloat = 1,
-//        alwaysShowSelectedBorder: Bool = false,
-//        showKeyboardOnAppear: Bool = true,
-//        keyboardDoneButtonText: String = "citpincode_keyboard_done_button_text".localized,
-//        cellSize: CGSize = .init(width: 40, height: 56),
-//        cellCornerRadius: CGFloat = 8,
-//        codeType: UIKeyboardType = .default,
-//        divider: CITPincodeDividerConfig = .none,
-//        resendButton: CITPincodeResendButtonConfig = .none
-//    ) {
-//        self.codeLength = codeLength
-//        self.font = font
-//        self.errorFont = errorFont ?? font
-    //        self.textColor = textColor
-    //        self.errorColor = errorColor
-    //        self.placeholder = String(placeholder.prefix(codeLength))
-    
-//        self.placeholderColor = placeholderColor ?? selectedBorderColor ?? textColor
-//        self.backgroundColor = backgroundColor
-//        self.selectedBackgroundColor = selectedBackgroundColor ?? backgroundColor
-//        self.selectedBorderColor = selectedBorderColor ?? backgroundColor
-//        self.selectedBorderWidth = selectedBorderWidth
-//        self.alwaysShowSelectedBorder = alwaysShowSelectedBorder
-//        self.showKeyboardOnAppear = showKeyboardOnAppear
-//        self.keyboardDoneButtonText = keyboardDoneButtonText
-//        self.cellSize = cellSize
-//        self.cellCornerRadius = cellCornerRadius
-//        self.codeType = codeType
-//        self.divider = divider
-//        self.resendButton = resendButton
-//    }
 }
 
 // MARK: - Resend Button
