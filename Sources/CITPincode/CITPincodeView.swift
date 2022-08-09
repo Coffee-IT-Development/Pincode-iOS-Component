d2c1e809a3ac0f9574ecc1072061eeaac379ec3f
Lex Brouwers <lexbrouwers@coffeeit.nl>
2022-08-09 13:12:47 +0200

    Move overlay to background, no difference otherwise

--- Sources/CITPincode/CITPincodeView.swift
+++ Sources/CITPincode/CITPincodeView.swift
@@ -64,7 +64,7 @@ public struct CITPincodeView: View {
                     }
                 }
             }
-            .overlay(
+            .background(
                 GeometryReader { proxy in
                     CITPincodeTextField(
                         text: $code,
