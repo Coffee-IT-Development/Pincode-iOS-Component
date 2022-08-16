93446629ba8ce7b6340a5c546eea43a76160b6b7
Lex Brouwers <lexbrouwers@coffeeit.nl>
2022-08-16 10:49:55 +0200

    Refactor to normal way of defining bindings

--- CITPincodeExample/CITPincodeExample/Customize/CITPincodeLabeledIntPicker.swift
+++ CITPincodeExample/CITPincodeExample/Customize/CITPincodeLabeledIntPicker.swift
@@ -11,13 +11,13 @@ import SwiftUI
 struct CITPincodeLabeledIntPicker: View {
     let label: String
     let range: ClosedRange<Int>
-    let value: Binding<Int>
+    @Binding var value: Int
     
     var body: some View {
         HStack {
             Text(label)
             Spacer()
-            Picker(label, selection: value) {
+            Picker(label, selection: $value) {
                 ForEach(range, id: \.self) {
                     Text(String($0))
                         .tag($0)
